d54ac76512dcef9f5ef6e380c9a05f0a41310cd3
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-27 23:56:28 +0300

    hono error handling

--- technews/server/index.ts
+++ technews/server/index.ts
@@ -1,9 +1,42 @@
-import { Context, Hono } from "hono";
+import type { ErrorResponse } from "@/shared/types";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
-app.get("/", (c: Context) => {
-  return c.json("Hello Hono!");
+app.get("/", (c) => {
+  return c.json({ message: "Hello world" });
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errResponse =
+      err.res ??
+      c.json<ErrorResponse>(
+        {
+          success: false,
+          error: err.message,
+          isFormError:
+            err.cause && typeof err.cause === "object" && "form" in err.cause
+              ? err.cause.form === true
+              : false,
+        },
+        err.status,
+      );
+
+    return errResponse;
+  }
+
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : (err.stack ?? err.message),
+    },
+    500,
+  );
 });
 
 export default app;
