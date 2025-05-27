ef425622afe57a3b3de61b91412f0cc50a82ca59
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-27 23:10:32 +0300

    setup drizzle

--- technews/server/index.ts
+++ technews/server/index.ts
@@ -3,5 +3,7 @@ import { Context, Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c: Context) => {
-  return c.text("Hello Hono!");
+  return c.json("Hello Hono!");
 });
+
+export default app;
