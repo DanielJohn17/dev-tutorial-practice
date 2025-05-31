0798f6ac991066a1432335a8cce8ef0bfedd0869
DanielJohn17 <danielyohannes38@gmail.com>
2025-06-01 01:19:05 +0300

    a simple REPL

--- pokedexcli/main.go
+++ pokedexcli/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+
+		tokens := cleanInput(input)
+		fmt.Println("Your command was:", tokens[0])
+	}
 }
 
 func cleanInput(text string) []string {
