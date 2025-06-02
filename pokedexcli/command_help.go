a171159a384410f913c66295ff05c2b1985c94d1
DanielJohn17 <danielyohannes38@gmail.com>
2025-06-02 20:06:03 +0300

    code refactor

---
+++ pokedexcli/command_help.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
+	fmt.Println()
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	return nil
+}
---
+++ pokedexcli/repl.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func start_repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+
+		tokens := cleanInput(input)
+
+		if len(tokens) == 0 {
+			continue
+		}
+
+		commands := getCommands()
+
+		cmd, ok := commands[tokens[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		if err := cmd.callback(); err != nil {
+			fmt.Printf("Error executing command '%s': %v\n", cmd.name, err)
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	text = strings.ToLower(strings.TrimSpace(text))
+	tokens := strings.Fields(text)
+	return tokens
+}
+
+func getCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
+	return commands
+}
