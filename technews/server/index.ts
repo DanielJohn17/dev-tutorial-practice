f4643cb6f9e477cf15a998bcc58cf70f4dca7192
DanielJohn17 <danielyohannes38@gmail.com>
2025-07-10 16:28:27 +0300

    Refactor: added session middleware and export apiRoute types

--- technews/server/index.ts
+++ technews/server/index.ts
@@ -3,7 +3,12 @@ import { auth } from "./utils/auth";
 import { HTTPException } from "hono/http-exception";
 import type { ErrorResponse } from "@/shared/types";
 
-const app = new Hono();
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+}>();
 
 app.get("/", (c: Context) => {
   return c.json({
@@ -12,37 +17,53 @@ app.get("/", (c: Context) => {
   });
 });
 
-app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    const errResponse =
-      err.res ??
-      c.json<ErrorResponse>(
-        {
-          success: false,
-          error: err.message,
-          isFormError:
-            err.cause && typeof err.cause === "object" && "form" in err.cause
-              ? err.cause.form === true
-              : false,
-        },
-        err.status,
-      );
-
-    return errResponse;
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
   }
 
-  return c.json<ErrorResponse>(
-    {
-      success: false,
-      error:
-        process.env.NODE_ENV === "production"
-          ? "Internal Server Error"
-          : (err.stack ?? err.message),
-    },
-    500,
-  );
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
 });
 
+const apiRoutes = app
+  .basePath("/api")
+  .on(["POST", "GET"], "/auth/**", (c) => auth.handler(c.req.raw))
+  .onError((err, c) => {
+    if (err instanceof HTTPException) {
+      const errResponse =
+        err.res ??
+        c.json<ErrorResponse>(
+          {
+            success: false,
+            error: err.message,
+            isFormError:
+              err.cause && typeof err.cause === "object" && "form" in err.cause
+                ? err.cause.form === true
+                : false,
+          },
+          err.status,
+        );
+
+      return errResponse;
+    }
+
+    return c.json<ErrorResponse>(
+      {
+        success: false,
+        error:
+          process.env.NODE_ENV === "production"
+            ? "Internal Server Error"
+            : (err.stack ?? err.message),
+      },
+      500,
+    );
+  });
+
 export default app;
+export type ApiRoutes = typeof apiRoutes;
