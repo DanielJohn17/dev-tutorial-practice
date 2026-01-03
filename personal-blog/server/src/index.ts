51323d0040fcce1c6a02fcfa813f0da360301bde
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-03 22:53:24 +0300

    Add: error Handling

--- personal-blog/server/src/index.ts
+++ personal-blog/server/src/index.ts
@@ -1,9 +1,38 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { ErrorResponse } from "@/shared/types";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errorResponse =
+      err.res ??
+      c.json<ErrorResponse>(
+        {
+          success: false,
+          error: err.message,
+        },
+        err.status,
+      );
+
+    return errorResponse;
+  }
+
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : (err.stack ?? err.message),
+    },
+    501,
+  );
+});
+
+export default app;
