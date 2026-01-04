b800e762396a8908fc835c260418f9f941259ec3
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-04 19:14:59 +0300

    Add: insert blog schema using drizzle zod

--- personal-blog/server/src/db/schema/blog.ts
+++ personal-blog/server/src/db/schema/blog.ts
@@ -1,5 +1,7 @@
 import { date, integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-zod";
+import z from "zod";
 
 import { user } from "@/db/schema/auth";
 import { favouriteBlogsTable } from "./favourite";
@@ -7,7 +9,7 @@ import { favouriteBlogsTable } from "./favourite";
 export const blogTable = pgTable("blogs", {
   id: integer("id").primaryKey(),
   title: varchar("title", { length: 256 }),
-  content: text("text"),
+  content: text("content"),
   createdAt: date("created_at", { mode: "string" }).defaultNow(),
   updatedAt: date("updated_at", { mode: "string" }).defaultNow(),
   authorId: text("author_id"),
@@ -25,3 +27,10 @@ export const blogRelations = relations(blogTable, ({ one, many }) => ({
   }),
   favourites: many(favouriteBlogsTable),
 }));
+
+export const insertBlogSchema = createInsertSchema(blogTable, {
+  title: z
+    .string()
+    .min(3, { error: "Title must be at least 3  characters long" }),
+  content: z.string(),
+});
--- personal-blog/shared/types.ts
+++ personal-blog/shared/types.ts
@@ -1,4 +1,11 @@
+import { insertBlogSchema } from "../server/src/db/schema/blog.ts";
+
 export type ErrorResponse = {
   success: false;
   error: string;
 };
+
+export const createBlogSchema = insertBlogSchema.pick({
+  title: true,
+  content: true,
+});
