f5ec4a378bfdf86e109d23e1a9e487684baa697e
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-05 00:57:10 +0300

    Add: schemas to better auth

--- personal-blog/server/src/utils/auth.ts
+++ personal-blog/server/src/utils/auth.ts
@@ -1,12 +1,17 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
+import { account, session, user, verification } from "@/db/schema/auth";
+
+const schema = { user, session, account, verification };
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: schema,
   }),
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: ["http://localhost:5000"],
 });
