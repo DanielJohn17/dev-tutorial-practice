f43af39a6c03aa66af4a7aacbbc2f180831a5b03
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-05 00:57:19 +0300

    Finish: create schema

--- personal-blog/server/src/routes/blog.ts
+++ personal-blog/server/src/routes/blog.ts
@@ -1,18 +1,36 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 
-import { createBlogSchema } from "@/shared/types";
+import { createBlogSchema, SuccessResponse } from "@/shared/types";
 import { Context } from "@/context";
+import { HTTPException } from "hono/http-exception";
+import { db } from "@/db";
+import { blogTable } from "@/db/schema/blog";
 
 export const blogRoute = new Hono<Context>().post(
   "/",
   zValidator("json", createBlogSchema),
-  (c) => {
+  async (c) => {
     const user = c.get("user");
     const session = c.get("session");
 
-    if (!user) return c.body(null, 401);
+    if (!user) throw new HTTPException(401, { message: "User not Found!" });
 
-    return c.json({ user: user, session: session });
+    const { title, content } = c.req.valid("json");
+
+    const [blog] = await db
+      .insert(blogTable)
+      .values({ title, content, authorId: user.id })
+      .returning({ id: blogTable.id });
+
+    if (!blog) {
+      throw new HTTPException(500, { message: "Failed to create blog" });
+    }
+
+    return c.json<SuccessResponse<{ blogId: number }>>({
+      success: true,
+      message: "Successfully created blog",
+      data: { blogId: blog.id },
+    });
   },
 );
