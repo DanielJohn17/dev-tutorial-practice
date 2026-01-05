a004cf5f565dd8637abac47b3b59f09986236e52
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-05 19:22:08 +0300

    Create: pagination and post types

--- personal-blog/shared/types.ts
+++ personal-blog/shared/types.ts
@@ -1,4 +1,5 @@
 import { insertBlogSchema } from "../server/src/db/schema/blog";
+import { z } from "zod";
 
 export type SuccessResponse<T = void> = {
   success: true;
@@ -10,7 +11,33 @@ export type ErrorResponse = {
   error: string;
 };
 
+export const paginationSchema = z.object({
+  page: z.number().optional().default(1),
+  limit: z.number().optional().default(10),
+});
+
 export const createBlogSchema = insertBlogSchema.pick({
   title: true,
   content: true,
 });
+
+export type Blog = {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    id: string;
+    username: string;
+  };
+  isFavourite: boolean;
+};
+
+export type PaginatedSuccessResponse<T> = {
+  pagination: {
+    page: number;
+    totalPages: number;
+  };
+  data: T;
+} & Omit<SuccessResponse, "data">;
