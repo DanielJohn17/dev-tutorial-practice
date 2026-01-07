8d10eefc641776c46d742c35315e563e173dec55
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-07 23:22:45 +0300

    Add: favourite blogs

--- personal-blog/server/src/routes/blog.ts
+++ personal-blog/server/src/routes/blog.ts
@@ -137,8 +137,81 @@ export const blogRoute = new Hono<Context>()
       200,
     );
   })
-      success: true,
-      message: "Successfully retrived blog",
-      data: blog as Blog,
+  .post("/:id/fav", zValidator("param", paramSchema), async (c) => {
+    const user = c.get("user");
+
+    if (!user) {
+      throw new HTTPException(401, { message: "User not found" });
+    }
+
+    const { id } = c.req.valid("param");
+
+    const [blog] = await db
+      .select({ count: countDistinct(blogTable.id) })
+      .from(blogTable)
+      .where(eq(blogTable.id, id))
+      .limit(1);
+
+    if (blog.count === 0) {
+      throw new HTTPException(404, { message: "Blog not found" });
+    }
+
+    const favourited = await db.transaction(async (tx) => {
+      const [isFavourited] = await tx
+        .select()
+        .from(favouriteBlogsTable)
+        .where(
+          and(
+            eq(favouriteBlogsTable.blogId, id),
+            eq(favouriteBlogsTable.userId, user.id),
+          ),
+        )
+        .limit(1);
+
+      if (isFavourited) {
+        const [unFavourite] = await tx
+          .delete(favouriteBlogsTable)
+          .where(
+            and(
+              eq(favouriteBlogsTable.blogId, id),
+              eq(favouriteBlogsTable.userId, user.id),
+            ),
+          )
+          .returning({
+            userId: favouriteBlogsTable.userId,
+            blogId: favouriteBlogsTable.blogId,
+          });
+
+        return {
+          message: "Successfully removed blog from favourite list",
+          data: unFavourite,
+        };
+      }
+
+      const [fave] = await tx
+        .insert(favouriteBlogsTable)
+        .values({ userId: user.id, blogId: id })
+        .returning({
+          userId: favouriteBlogsTable.userId,
+          blogId: favouriteBlogsTable.blogId,
+        });
+
+      if (!fave) {
+        throw new HTTPException(500, { message: "Failed to favourite post" });
+      }
+
+      return {
+        message: "Successfully added blog to favourite list",
+        data: fave,
+      };
     });
+
+    return c.json<SuccessResponse<{ userId: string; blogId: number }>>(
+      {
+        success: true,
+        message: favourited.message,
+        data: favourited.data,
+      },
+      201,
+    );
   });
