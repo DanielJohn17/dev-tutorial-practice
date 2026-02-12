81c0fe85d7fc4ce285fb17272eab715fe2cf5961
DanielJohn17 <danielyohannes38@gmail.com>
2026-02-12 14:52:14 +0300

    TCPtoHTTP: Connect the Parsing

--- tcp_to_http/cmd/tcplistener/main.go
+++ tcp_to_http/cmd/tcplistener/main.go
@@ -1,50 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"tcptohttp.danieljohn17/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		str := ""
-		for {
-			data := make([]byte, 8)
-
-			_, err := f.Read(data)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-			}
-
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-
-				out <- str
-				data = data[i+1:]
-				str = ""
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp4", ":42069")
@@ -60,8 +22,15 @@ func main() {
 			log.Fatal("connection error", err)
 		}
 
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+
+			log.Fatal("connection error", err)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- target: %s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- method: %s\n", req.RequestLine.Method)
+		fmt.Printf("- version: %s\n", req.RequestLine.HttpVersion)
 	}
 }
