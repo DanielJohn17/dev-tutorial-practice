b76d822bd3c1a09bd55432d9530af8af159056fa
DanielJohn17 <danielyohannes38@gmail.com>
2026-02-13 12:29:00 +0300

    TCPtoHTTP: Multiple Values

--- tcp_to_http/internal/headers/headers_test.go
+++ tcp_to_http/internal/headers/headers_test.go
@@ -19,6 +19,18 @@ func TestHeadersParse(t *testing.T) {
 	assert.Equal(t, 45, n)
 	assert.True(t, done)
 
+	// Test: Multiple values
+	headers = NewHeaders()
+	data = []byte(
+		"Host:localhost:42069\r\nSet-Person: lane-loves-go\r\nSet-Person: prime-loves-zig\r\nSet-Person: tj-loves-ocaml\r\n\r\n",
+	)
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	require.Equal(t, "lane-loves-go, prime-loves-zig, tj-loves-ocaml", headers.Get("Set-Person"))
+	require.Equal(t, 108, n)
+	require.True(t, done)
+
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host : localhost:42069       \r\n\r\n")
@@ -27,6 +39,7 @@ func TestHeadersParse(t *testing.T) {
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
+	// Test: Invalid name token
 	headers = NewHeaders()
 	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
